75d26315e37e2f6aad8c10020afae016c048f6ac
Endzelisp <endzelisp@gmail.com>
2023-02-13 23:03:16 -0400

    Redesign the whole webpage to allow new functionalities

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
-// Const and variables declarations
+// // Const and variables declarations
 
-const inputTextFieldEl = document.querySelector('.input-area input');
-const addButtonEl = document.querySelector('.input-area #add');
+// const inputTextFieldEl = document.querySelector('.container input');
+// const addButtonEl = document.querySelector('.container #add');
 
-// Functions
+// // Functions
 
-function getInputText (inputField) {
-  const inputContent = inputField.value
-  if (inputContent === '') {
-    inputField.focus()
-    return null
-  }
-  inputField.value = ''
-  inputField.focus()
-  return inputContent
-};
+// function getInputText (inputField) {
+//   const inputContent = inputField.value
+//   if (inputContent === '') {
+//     inputField.focus()
+//     return null
+//   }
+//   inputField.value = ''
+//   inputField.focus()
+//   return inputContent
+// };
 
-function createNewitem (item) {
-  if (item === null || item === '') return
+// function createNewitem (item) {
+//   if (item === null || item === '') return
 
-  const listItem = document.createElement('li');
-  const para = document.createElement('p');
-  const span = document.createElement('span');
+//   const listItem = document.createElement('li');
+//   const para = document.createElement('p');
+//   const span = document.createElement('span');
 
-  para.textContent = item;
-  span.textContent = '❌';
-  span.classList.add('delete-cross')
-  listItem.appendChild(para);
-  listItem.appendChild(span);
-  return listItem;
-};
+//   para.textContent = item;
+//   span.textContent = '❌';
+//   span.classList.add('delete-cross')
+//   listItem.appendChild(para);
+//   listItem.appendChild(span);
+//   return listItem;
+// };
 
-// Local storage functions
+// // Local storage functions
 
-let arrayList = []
+// let arrayList = []
 
-function readFromLocal () {
-  let tempArr = localStorage.getItem('key');
-  if (tempArr === null) return
-  tempArr = tempArr.split(',')  
-  if (tempArr !== []) {
-    arrayList = tempArr;
-  }
-}
+// function readFromLocal () {
+//   let tempArr = localStorage.getItem('key');
+//   if (tempArr === null) return
+//   tempArr = tempArr.split(',')  
+//   if (tempArr !== []) {
+//     arrayList = tempArr;
+//   }
+// }
 
-function displayList (arr) {
-  const itemsNodeList = document.querySelector('.items ul');
-  if (arr !== []) {
-    for (let i of arr) {
-      if (i === '') return
-      itemsNodeList.appendChild(createNewitem(i));
-    }
-  }
-}
+// function displayList (arr) {
+//   const itemsNodeList = document.querySelector('.items ul');
+//   if (arr !== []) {
+//     for (let i of arr) {
+//       if (i === '') return
+//       itemsNodeList.appendChild(createNewitem(i));
+//     }
+//   }
+// }
 
-function writeLocal (item) {
-  if (arrayList[0] === '') {
-    arrayList[0] = item;
-    localStorage.setItem('key', arrayList)
-    return
-  }
-  arrayList.push(item);
-  localStorage.setItem('key', arrayList)
-}
+// function writeLocal (item) {
+//   if (arrayList[0] === '') {
+//     arrayList[0] = item;
+//     localStorage.setItem('key', arrayList)
+//     return
+//   }
+//   arrayList.push(item);
+//   localStorage.setItem('key', arrayList)
+// }
 
-function deleteFromLocal (item) {
-  if (arrayList.includes(item)) {
-    let index = arrayList.indexOf(item);
-    arrayList.splice(index, 1);
-  }
-  localStorage.setItem('key', arrayList)
-}
+// function deleteFromLocal (item) {
+//   if (arrayList.includes(item)) {
+//     let index = arrayList.indexOf(item);
+//     arrayList.splice(index, 1);
+//   }
+//   localStorage.setItem('key', arrayList)
+// }
 
-readFromLocal()
-displayList(arrayList)
+// readFromLocal()
+// displayList(arrayList)
 
-// Event listeners
+// // Event listeners
 
-function addNewItem () {
-  const itemsNodeList = document.querySelector('.items ul');
-  let inputText = getInputText(inputTextFieldEl);
-  if (inputText === '') return
-  writeLocal(inputText);
-  itemsNodeList.appendChild(createNewitem(inputText));
-}
+// function addNewItem () {
+//   const itemsNodeList = document.querySelector('.items ul');
+//   let inputText = getInputText(inputTextFieldEl);
+//   if (inputText === '') return
+//   writeLocal(inputText);
+//   itemsNodeList.appendChild(createNewitem(inputText));
+// }
 
 
-addButtonEl.addEventListener('click', addNewItem);
-inputTextFieldEl.addEventListener('keydown', (e) => {
-  if (e.keyCode === 13) {
-    addNewItem()
-  }
-})
+// addButtonEl.addEventListener('click', addNewItem);
+// inputTextFieldEl.addEventListener('keydown', (e) => {
+//   if (e.keyCode === 13) {
+//     addNewItem()
+//   }
+// })
 
-// event listener to delete items
+// // event listener to delete items
 
-document.addEventListener('click', (e) => {
-  let cross = e.target;
-  if (cross.className === 'delete-cross') {
-    cross.parentNode.remove()
-    let itemToDelete = cross.parentNode.firstChild.textContent;
-    deleteFromLocal(itemToDelete)
-  }
-})
+// document.addEventListener('click', (e) => {
+//   let cross = e.target;
+//   if (cross.className === 'delete-cross') {
+//     cross.parentNode.remove()
+//     let itemToDelete = cross.parentNode.firstChild.textContent;
+//     deleteFromLocal(itemToDelete)
+//   }
+// })
