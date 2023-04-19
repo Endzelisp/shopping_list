11f7f6f86067e2efe6d5fff84c7f0db32ae03829
Endzelisp <endzelisp@gmail.com>
2023-04-19 18:31:09 -0400

    Create an independent module to store all custom events

---
+++ custom-events-module.js
@@ -0,0 +1,27 @@
+export const updateList = new CustomEvent("updateList", {
+  detail: {},
+  bubbles: true,
+  cancelable: false,
+  composed: false,
+});
+
+export const renderList = new CustomEvent("renderList", {
+  detail: {},
+  bubbles: true,
+  cancelable: false,
+  composed: false,
+});
+
+export const renderTotalUSD = new CustomEvent("renderTotalUSD", {
+  detail: {},
+  bubbles: true,
+  cancelable: false,
+  composed: false,
+});
+
+export const renderTotalBs = new CustomEvent("renderTotalBs", {
+  detail: {},
+  bubbles: true,
+  cancelable: false,
+  composed: false,
+});
