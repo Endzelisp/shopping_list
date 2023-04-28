9ada2cf1cd154ccf7c8f1faeb28e0168d7f8f2a2
Endzelisp <endzelisp@gmail.com>
2023-04-28 17:49:08 -0400

    Rename some const to more descriptive names and consistency

--- modules/ui.js
+++ modules/ui.js
@@ -4,19 +4,26 @@ function _getElem(selector) {
   return document.querySelector(selector);
 }
 
-export const mainContainer = _getElem('[data-container="data-container"]');
-export const exchangeRateEl = _getElem('[data-rate="exchange-rate"]');
-export const totalPriceBs = _getElem('[data-total="total-bs"]');
-export const totalPriceUSD = _getElem('[data-total="total-usd"]');
-export const newItem = _getElem('[data-button="new-item"]');
-export const newWeightedItem = _getElem('[data-button="weighted-item"]');
-export const itemContainer = _getElem('[data-container="main-list"]');
-export const dialogExchangeRate = _getElem('[data-dialog="exchange-rate"]');
-export const dialogNewItem = _getElem('[data-dialog="new-item"]');
-export const dialogNewweightedItem = _getElem('[data-dialog="weighted-item"]');
-export const template = _getElem('[data-template="new-item"]');
-
-export const clearList = function () {
-  const list = itemContainer.querySelectorAll("new-product");
+export function clearList() {
+  const list = itemContainer.querySelectorAll("new-item");
   list.forEach((item) => item.remove());
-};
+}
+
+// header
+export const exchangeRateElem = _getElem('[data-element="exchange-rate"]');
+export const totalBsElem = _getElem('[data-element="total-bs"]');
+export const totalUSDElem = _getElem('[data-element="total-usd"]');
+export const addItemBtn = _getElem('[data-button="unitary-item"]');
+export const addWeightedItem = _getElem('[data-button="weighted-item"]');
+
+// containers
+export const mainContainer = _getElem('[data-container="main"]');
+export const itemContainer = _getElem('[data-container="list"]');
+
+// dialogs
+export const dialogExchangeRate = _getElem('[data-dialog="exchange-rate"]');
+export const dialogUnitaryItem = _getElem('[data-dialog="unitary-item"]');
+export const dialogWeightedItem = _getElem('[data-dialog="weighted-item"]');
+
+// templates
+export const newItemTemplate = _getElem('[data-template="new-item"]');
