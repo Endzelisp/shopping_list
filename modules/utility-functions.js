eb349f52b252157e43df62741b8c21ff872353b1
Endzelisp <endzelisp@gmail.com>
2023-05-12 18:25:56 -0400

    Delete unnecessary function

--- modules/utility-functions.js
+++ modules/utility-functions.js
@@ -4,11 +4,3 @@ export function generateId(product) {
   const word = product.split(" ")[0];
   return randomStart + word + randomEnd;
 }
-
-export function calculateTotal(item) {
-  if (item.type === "unitary") {
-    return parseFloat(item.price) * parseInt(item.quantity);
-  } else if (item.type === "weighted") {
-    return parseFloat(item.price) * parseFloat(item.weight);
-  }
-}
