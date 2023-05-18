ac468310fbb50a4e59edb0af08c7dfa813cf6b75
Endzelisp <endzelisp@gmail.com>
2023-05-17 22:03:23 -0400

    Write descriptive JSDocs comments for each function

--- modules/utility-functions.js
+++ modules/utility-functions.js
@@ -1,3 +1,10 @@
+/**
+ * Generates a unique ID for a given product.
+ *
+ * @param {string} product - The name of the product to generate an ID for.
+ * @returns {string} Returns a unique ID string for the product.
+ *
+ */
 export function generateId(product) {
   const randomStart = Math.random().toString().slice(-3);
   const randomEnd = Math.random().toString().slice(-3);
@@ -5,9 +12,22 @@ export function generateId(product) {
   return randomStart + word + randomEnd;
 }
 
+/**
+ * Checks if the input is a valid product
+ *
+ * @param {string} product The product string to validate
+ * @returns {boolean} Returns true if the input is a non-empty string, and false otherwise
+ *
+ */
 export const isValidProduct = (product) =>
   product !== "" && typeof product === "string";
 
+/**
+ * Check if the data-type is a number and if it is greater than zero
+ *
+ * @param {number} num The input number to check
+ * @returns {boolean} Returns true if the input is a positive number, and false otherwise
+ */
 export const isValidNumber = (num) => !isNaN(num) && num > 0;
 
 /**
@@ -16,5 +36,3 @@ export const isValidNumber = (num) => !isNaN(num) && num > 0;
  * @param {number} floatNum Floating-point number to be limited
  */
 export const roundToTwo = (floatNum) => Math.round(floatNum * 100) / 100;
-
-//limitDecimal
